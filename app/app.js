67b4e215d13f13881b8f770466cee26c251024e8
Prasannaa Santhanam <prasannaa.s@gmail.com>
2018-05-14 16:35:22 -0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -5,16 +5,26 @@ angular.module('oneDrivePickerApp', ['oneDrivePickerModule'])
             clientId: 'bad2f6c6-15ec-4c98-ace0-1174419ee9ce'
         });
     }])
-    .controller('OneDrivePickerCtrl', ['$scope', 'oneDrivePickerService',
-        function ($scope, oneDrivePickerService) {
-            $scope.selectedFiles = [];
-            $scope.onFilePicked = function(files) {
-                 $scope.$apply(function() {
-                    $scope.selectedFiles = oneDrivePickerService.getFileInfos(files);
-                });            
+    .controller('$log', 'OneDrivePickerCtrl', ['$scope', 'oneDrivePickerService',
+        function ($log, $scope, oneDrivePickerService) {
+            $scope.selectedFiles = [{name: 'NA', url: 'NA'}];
+
+            var onError = function(error) {
+                $log.error('OneDrive Error:', error);
             };
+
+            var onCanceled = function() {
+                $log.error('OneDrive Cancelled by user');  
+            };
+
+            $scope.onFilePicked = function(files) {
+                $scope.$apply(function() {
+                    $scope.selectedFiles = oneDrivePickerService.getFileInfos(files);
+                });
+            };
+
             $scope.onPickFile = function() {
-                oneDrivePickerService.pickAFile($scope.onFilePicked);
+                oneDrivePickerService.pickAFile($scope.onFilePicked, onCanceled, onError);
             };
         }
     ]);
